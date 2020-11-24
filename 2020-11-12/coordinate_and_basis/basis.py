3020f415832731dd0eeb06c0e878002fd45c202d
conf8o <8confeito8@gmail.com>
2020-11-24 15:20:34 +0900

    プロットをplotlyからmatplotlibに変更

--- 2020-11-12/coordinate_and_basis/basis.py
+++ 2020-11-12/coordinate_and_basis/basis.py
@@ -1,6 +1,6 @@
 import numpy as np
 import numpy.linalg as LA
-import plotly.graph_objects as go
+import matplotlib.pyplot as plt
 
 def _linearly_independent(*vecs):
     m = np.stack(vecs)
@@ -15,22 +15,26 @@ class Basis:
     def coordinate(self, vec):
         return self.basis_mat.dot(vec)
     
-    def space_figure(self, n=10):
+    def plot_coordinate(self, p, n=10):
         assert self.dimension == 2, "method 'space_figure' is implemented for only 2 dimension."
-        
-        fig = go.Figure()
-        fig.update_xaxes(range=[0, n])
-        fig.update_yaxes(range=[0, n])
+
+        plt.xlim(0, n)
+        plt.ylim(0, n)
 
         for x in range(-n, n):
             start = self.coordinate([x, -n])
             end = self.coordinate([x, n])
-            fig.add_shape(type="line",
-                           x0=start[0], y0=start[1], x1=end[0], y1=end[1])
+
+            xs = np.linspace(start[0], end[0])
+            ys = np.linspace(start[1], end[1])
+            plt.plot(xs, ys, color="#AAAAAA")
             
         for y in range(-n, n):
             start = self.coordinate([-n, y])
             end = self.coordinate([n, y])
-            fig.add_shape(type="line",
-                          x0=start[0], y0=start[1], x1=end[0], y1=end[1])
-        return fig
+
+            xs = np.linspace(start[0], end[0])
+            ys = np.linspace(start[1], end[1])
+            plt.plot(xs, ys, color="#AAAAAA")
+
+        plt.plot(*p, marker="o")
